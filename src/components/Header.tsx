0d0cae386e74ea2d27a45566e4b7fd6c26d4b281
Ankit006 <ankitghosh311@outlook.com>
2026-05-04 23:41:16 +0530

    [update]: added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,20 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Button } from "./ui/button"
+
 export default function Header() {
-  return <header>this is a header element</header>
+  return (
+    <header className="flex items-center justify-between border-b border-gray-200 px-8 py-3">
+      <h1 className="font-heading text-xl font-semibold">Next Form</h1>
+      <div className="flex items-center gap-2">
+        <Button className="border-2 border-solid border-primary bg-transparent text-lg text-primary duration-300 hover:text-white">
+          Preview
+        </Button>
+        <Button className="text-lg">Publish</Button>
+        <Avatar className="h-12 w-12">
+          <AvatarImage src="https://placehold.co/800x600" />
+          <AvatarFallback>Placeholder image</AvatarFallback>
+        </Avatar>
+      </div>
+    </header>
+  )
 }
