bda9b325f974893b171a67072db8a099d5013863
Ankit006 <ankitghosh311@outlook.com>
2026-05-09 13:56:05 +0000

    [update]: fix formatting issues

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@
 import { tanstackConfig } from "@tanstack/eslint-config"
 
 export default [...tanstackConfig, {
-    ignores: ["src/components/ui/**"]
+    ignores: ["src/components/ui/**", ".output/**"]
 }]
